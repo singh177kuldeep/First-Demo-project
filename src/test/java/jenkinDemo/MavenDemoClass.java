6ec9664b8ec7350048b3c5fb33b5ce261a2a2dc3
singh177kuldeep <singh177kuldeep@gmail.com>
2024-07-11 14:33:54 +0530

    adding maven files to github

---
+++ src/test/java/jenkinDemo/MavenDemoClass.java
@@ -0,0 +1,13 @@
+package jenkinDemo;
+
+import org.testng.annotations.Test;
+
+public class MavenDemoClass {
+
+	@Test
+	public void test() {
+
+		System.out.println("Hello, this is maven project");
+	}
+
+}
